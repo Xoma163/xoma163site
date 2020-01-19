be8fe1a23a2d5ee87c9dd5806c9e20e0ace61f38
Xoma163 <Xoma163rus@gmail.com>
2020-01-19 11:06:38 +0400

    Выполнение команды с Lock и обратно вернул всё в один метод

--- apps/API_VK/command/commands/Games/Petrovich.py
+++ apps/API_VK/command/commands/Games/Petrovich.py
@@ -1,7 +1,10 @@
 import datetime
+from threading import Lock
 
 from apps.API_VK.command.CommonCommand import CommonCommand
 from apps.games.models import PetrovichGames, PetrovichUser
+
+lock = Lock()
 
 
 class Petrovich(CommonCommand):
@@ -11,39 +14,37 @@ class Petrovich(CommonCommand):
         super().__init__(names, help_text, for_conversations=True)
 
     def start(self):
-        return self.start_real(self.vk_event.chat)
+        with lock:
+            today = datetime.datetime.now()
+            winner_today = PetrovichGames.objects.filter(date__year=today.year,
+                                                         date__month=today.month,
+                                                         date__day=today.day,
+                                                         chat=self.vk_event.chat).last()
+            if winner_today is not None:
+                if winner_today.user.name in ["Евгений", "Женя"]:
+                    return "Женя дня - %s" % winner_today.user
+                else:
+                    return "Петрович дня - %s" % winner_today.user
 
-    @staticmethod
-    def start_real(chat):
-        today = datetime.datetime.now()
-        winner_today = PetrovichGames.objects.filter(date__year=today.year,
-                                                     date__month=today.month,
-                                                     date__day=today.day,
-                                                     chat=chat).last()
-        if winner_today is not None:
-            if winner_today.user.name in ["Евгений", "Женя"]:
-                return "Женя дня - %s" % winner_today.user
+            # order_by ? = random
+            winner = PetrovichUser.objects.filter(chat=self.vk_event.chat, active=True).order_by("?").first()
+            if winner:
+                winner = winner.user
             else:
-                return "Петрович дня - %s" % winner_today.user
+                return "Нет участников игры. Зарегистрируйтесь! /рег"
 
-        # order_by ? = random
-        winner = PetrovichUser.objects.filter(chat=chat, active=True).order_by("?").first()
-        if winner:
-            winner = winner.user
-        else:
-            return "Нет участников игры. Зарегистрируйтесь! /рег"
-
-        PetrovichGames.objects.filter(chat=chat).delete()
-        new_winner = PetrovichGames()
-        new_winner.user = winner
-        new_winner.chat = chat
-        new_winner.save()
-        winner_petrovich = PetrovichUser.objects.filter(user=winner, chat=chat).first()
-        winner_petrovich.wins = int(winner_petrovich.wins) + 1
-        winner_petrovich.save()
-        messages = ["Такс такс такс, кто тут у нас"]
-        who = "Петрович"
-        if winner.name in ["Евгений", "Женя"]:
-            who = "Женя"
-        messages.append("Наш сегодняшний {} дня - [{}|{} {}]".format(who, winner.nickname, winner.name, winner.surname))
-        return messages
+            PetrovichGames.objects.filter(chat=self.vk_event.chat).delete()
+            new_winner = PetrovichGames()
+            new_winner.user = winner
+            new_winner.chat = self.vk_event.chat
+            new_winner.save()
+            winner_petrovich = PetrovichUser.objects.filter(user=winner, chat=self.vk_event.chat).first()
+            winner_petrovich.wins = int(winner_petrovich.wins) + 1
+            winner_petrovich.save()
+            messages = ["Такс такс такс, кто тут у нас"]
+            who = "Петрович"
+            if winner.name in ["Евгений", "Женя"]:
+                who = "Женя"
+            messages.append(
+                "Наш сегодняшний {} дня - [{}|{} {}]".format(who, winner.nickname, winner.name, winner.surname))
+            return messages
