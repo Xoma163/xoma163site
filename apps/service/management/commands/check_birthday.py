cee2c425f80a95652746127946c73e81e33ca5c4
Xoma163 <Xoma163rus@gmail.com>
2020-03-28 09:18:20 +0400

    Фикс др

--- apps/service/management/commands/check_birthday.py
+++ apps/service/management/commands/check_birthday.py
@@ -11,19 +11,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         chat_ids = options['chat_id'][0].split(',')
         for chat_id in chat_ids:
-            chat = VkChat.objects.filter(chat_id=vk_bot.get_group_id(chat_id))
+            chat = VkChat.objects.filter(chat_id=vk_bot.get_group_id(chat_id)).first()
 
             if not chat:
                 print(f"Чата с id = {chat_id} не существует")
                 break
-            chat = chat.first()
 
             today = datetime.now()
-            users = VkUser.objects.filter(chats__in=chat,
+            users = VkUser.objects.filter(chats=chat,
                                           birthday__day=today.day,
                                           birthday__month=today.month)
 
             for user in users:
+                # ToDo: mention.
+                # ToDo: common method get_mention
                 vk_bot.send_message(chat.chat_id, f"С Днём рождения, {user.name}!")
 
     def add_arguments(self, parser):
