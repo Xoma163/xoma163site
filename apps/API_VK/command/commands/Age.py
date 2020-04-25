c8aa7992b41589f86e1d0156c1a72c62be8efc87
Xoma163 <Xoma163rus@gmail.com>
2020-04-25 14:00:20 +0400

    Скейлинг размеров для разных размеров картинок

--- apps/API_VK/command/commands/Age.py
+++ apps/API_VK/command/commands/Age.py
@@ -17,13 +17,23 @@ def draw_on_images(image_url, faces):
     thickness = {'big': 6, 'medium': 2, 'small': 1}
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = {'big': 1, 'medium': 0.8, 'small': 0.6}
+    shift_age_point = [35, 10]
 
+    width, height, channels = _image.shape
+    scale = width * height / 1920 / 1080
+    scale = max(scale, 0.8)
+    for x in font_scale:
+        font_scale[x] *= scale
+    for x in thickness:
+        thickness[x] = round(thickness[x] * scale)
+    for i in range(len(shift_age_point)):
+        shift_age_point[i] = round(shift_age_point[i] * scale)
     for face in faces:
         start_point = (int(face['bbox'][0]), int(face['bbox'][1]))
         end_point = (int(face['bbox'][2]), int(face['bbox'][3]))
         if 'age' in face:
             age = str(round(face['age']))
-            age_point = (int(face['bbox'][2]) - 35, int(face['bbox'][3]) - 10)
+            age_point = (int(face['bbox'][2]) - shift_age_point[0], int(face['bbox'][3]) - shift_age_point[1])
             _image = cv2.rectangle(_image, start_point, end_point, color['red'], thickness['medium'])
             _image = cv2.putText(_image, age, age_point, font, font_scale['small'], color['black'], thickness['big'])
             _image = cv2.putText(_image, age, age_point, font, font_scale['small'], color['white'], thickness['medium'])
