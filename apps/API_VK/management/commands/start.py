b5cd5a0ffdda92fc7388d489cfa7d654b561c93d
Xoma163 <Xoma163rus@gmail.com>
2020-04-29 18:29:00 +0400

    Улучшил разработку, а именно дебаг Петровича, который реагирует только на меня и в моей конфе. Отказ от thread.lock.

--- apps/API_VK/management/commands/start.py
+++ apps/API_VK/management/commands/start.py
@@ -1,10 +1,7 @@
 #!/var/www/xoma163.site/venv/bin/python -u
 
-import os
-
 from django.core.management import BaseCommand
 
-from xoma163site.settings import BASE_DIR
 from xoma163site.wsgi import cameraHandler, vk_bot
 
 
@@ -14,8 +11,15 @@ class Command(BaseCommand):
         super().__init__()
 
     def handle(self, *args, **kwargs):
-        if not os.path.exists(BASE_DIR + '/thread.lock'):
-            open(BASE_DIR + '/thread.lock', 'w')
+        debug = kwargs['debug']
+        if debug:
+            vk_bot.DEVELOP_DEBUG = True
+            vk_bot.start()
+            print("BOT STARTED")
+        else:
             vk_bot.start()
             cameraHandler.start()
             print("BOT AND CAMERA HANDLER STARTED")
+
+    def add_arguments(self, parser):
+        parser.add_argument('debug', type=bool, nargs='?', help='debug', default=False)
