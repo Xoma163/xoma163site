4851fe8367b3a59ff87439d45a251ff8ccb128cd
Xoma163 <Xoma163rus@gmail.com>
2020-06-09 00:05:50 +0400

    added migrations

--- config/setup/setup.sh
+++ config/setup/setup.sh
@@ -1,6 +1,6 @@
 # Install python, python-venv and project requirements
 sudo apt -y update
-sudo apt install -y python3.7 python3.7-venv python3-venv postgresql psycopg2 psycopg2-binary libpq-dev
+sudo apt install -y python3.7 python3.7-venv python3-venv postgresql libpq-dev ffmpeg
 python3.7 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
