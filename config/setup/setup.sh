f5417999ce8030312c3072ff88d433da46f3a26d
Xoma163 <Xoma163rus@gmail.com>
2020-06-09 21:49:30 +0400

    Допил конфига setup.sh

--- config/setup/setup.sh
+++ config/setup/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Install python, python-venv and project requirements
+# install python, python-venv and project requirements
 sudo apt -y update
 sudo apt install -y python3.7 python3.7-venv python3-venv python3.7-dev python3-wheel postgresql libpq-dev ffmpeg nginx build-essential
 
@@ -10,17 +10,26 @@ pip install wheel
 pip install --upgrade --user travis pip setuptools wheel virtualenv
 pip install -r requirements.txt
 
+# rename abs paths
+old_path="/var/www/xoma163site/"
+sed -i "s#$old_path#$PWD/#g" ./config/xoma163bot.service
+sed -i "s#$old_path#$PWD/#g" ./config/xoma163site.service
+sed -i "s#$old_path#$PWD/#g" ./config/xoma163site_nginx.conf
+
 # systemd
-sudo ln -s "$(pwd)/config/xoma163bot.service" /etc/systemd/system/
-sudo ln -s "$(pwd)/config/xoma163site.service" /etc/systemd/system/
+rm /etc/systemd/system/xoma163bot.service || echo $
+rm /etc/systemd/system/xoma163site.service || echo $
+sudo ln -s "$PWD/config/xoma163bot.service" /etc/systemd/system/ || echo $
+sudo ln -s "$PWD/config/xoma163site.service" /etc/systemd/system/ || echo $
 sudo systemctl daemon-reload
 
-cp secrets/secrets_example.py secrets/secrets.py
-
-#web
-sudo ln -s "$(pwd)/config/xoma163site_nginx.conf" /etc/nginx/sites-available/
-sudo ln -s "$(pwd)/config/xoma163site_nginx.conf" /etc/nginx/sites-enabled/
+# web
+rm /etc/nginx/sites-available/xoma163site_nginx.conf || echo $
+rm /etc/nginx/sites-enabled/xoma163site_nginx.conf || echo $
+sudo ln -s "$PWD/config/xoma163site_nginx.conf" /etc/nginx/sites-available/ || echo $
+sudo ln -s "$PWD/config/xoma163site_nginx.conf" /etc/nginx/sites-enabled/ || echo $
 sudo systemctl restart nginx
 
+# migrations
 python manage.py migrate
 python manage.py initial
